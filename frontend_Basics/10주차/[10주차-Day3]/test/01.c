1f655df15e26f96a5f3390271fe7e64de484903d
JiwooPaeng <shishalu04@gmail.com>
2024-06-14 22:52:54 +0900

    [Week10-Day3] 자료형, 변수 및 상수의 이해

---
+++ frontend_Basics/10주차/[10주차-Day3]/test/01.c
@@ -0,0 +1,12 @@
+// 01. 사용자로부터 1개의 정수를 입력 받아 입력 받은 수에 5를 곱한 결과를 출력하라.
+
+#include <stdio.h>
+
+int main()
+{
+    int input = 0;
+    scanf("%d", &input);
+    printf("result : %d", input * 5);
+
+    return 0;
+}
---
+++ frontend_Basics/10주차/[10주차-Day3]/test/02.c
@@ -0,0 +1,11 @@
+// 2. 사용자로부터 알파벳 문자 하나를 입력 받는다. 그리고 이에 해당하는 숫자를 출력하라.
+// 예를 들어, 'A'를 입력하면 출력 결과는 65가 되어야 한다.
+
+int main()
+{
+    char input;
+    scanf("%c", &input);
+    printf("result : %d", input);
+
+    return 0;
+}
