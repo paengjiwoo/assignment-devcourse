7d8a24acf003bfae51c29b1b1a49b1a6483b6c7f
paengjiwoo <shishalu04@gmail.com>
2024-08-05 14:29:35 +0900

    [Week15-Day5] login 상태 관리 및 도서 목록 구현 추가

--- frontend_BookShop/15주차/[15주차-Day5]/book-store/src/api/auth.api.ts
+++ frontend_BookShop/15주차/[15주차-Day5]/book-store/src/api/auth.api.ts
@@ -5,3 +5,22 @@ export const signUp = async(userData: SignupProps) => {
   const response = await httpClient.post("/user/join", userData);
   return response.data;
 }
+
+export const resetRequested = async (data: SignupProps) => {
+  const response = await httpClient.post("/user/reset", data);
+  return response.data;
+}
+
+export const resetPassword = async (data: SignupProps) => {
+  const response = await httpClient.put("/users/reset", data);
+  return response.data;
+}
+
+interface LoginResponse {
+  token: string;
+}
+
+export const login = async (data: SignupProps) => {
+  const response = await httpClient.post<LoginResponse>("/users/login", data);
+  return response.data;
+}
--- frontend_BookShop/15주차/[15주차-Day5]/book-store/src/pages/ResetPassword.tsx
+++ frontend_BookShop/15주차/[15주차-Day5]/book-store/src/pages/ResetPassword.tsx
@@ -5,23 +5,19 @@ import Button from "../components/common/Button";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { signUp } from "../api/auth.api";
+import { resetPassword, resetRequested, signUp } from "../api/auth.api";
 import { useAlert } from "../hooks/useAlert";
+import { SignupStyle } from "./Signup";
 
 export interface SignupProps {
   email: string;
   password: string;
 }
 
-const Signup = () => {
+const ResetPassword = () => {
   const navigate = useNavigate();
   const showAlert = useAlert();
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-
-  // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-  //   event.preventDefault()
-  // }
+  const [resetRequest, setResetRequest] = useState<boolean>(false);
 
   const { 
     register, 
@@ -30,15 +26,21 @@ const Signup = () => {
     useForm<SignupProps>();
 
   const onSubmit = (data: SignupProps) => {
-    signUp(data).then(res => {
-      showAlert('회원가입이 완료되었습니다.');
-      navigate("/login");
-    })
+    if (resetRequest) {
+      resetPassword(data).then(() => {
+        showAlert("비밀번호가 초기화 되었습니다.")
+        navigate("/login");
+      })
+    } else {
+      resetRequested(data).then(() => {
+        setResetRequest(true);
+      })
+    }
   };
 
   return(
     <>
-      <Title size="large">회원가입</Title>
+      <Title size="large">비밀번호 초기화</Title>
       <SignupStyle>
         <form onSubmit={handleSubmit(onSubmit)}>
           <fieldset>
@@ -51,6 +53,7 @@ const Signup = () => {
               <p className="error-text">이메일을 입력해주세요.</p>
             }
           </fieldset>
+          {resetRequest && (
           <fieldset>
             <InputText 
               placeholder="비밀번호" 
@@ -61,9 +64,10 @@ const Signup = () => {
               <p className="error-text">비밀번호를 입력해주세요.</p>
             }
           </fieldset>
+          )}
           <fieldset>
             <Button type="submit" size="medium" schema="primary">
-              회원가입
+              {resetRequest ? "비밀번호 초기화" : "초기화 요청"}
             </Button>
           </fieldset>
 
@@ -76,30 +80,4 @@ const Signup = () => {
   );
 }
 
-export const SignupStyle = styled.div`
-  max-width: ${({ theme }) => theme.layout.width.small};
-  margin: 80px auto;
-
-  fieldset {
-    border: 0;
-    padding: 0 0 8px 0;
-    .error-text {
-      color: red;
-    }
-  }
-
-  input {
-    width: 100%;
-  }
-
-  button {
-    width: 100%;
-  }
-
-  .info {
-    text-align: center;
-    padding: 16px 0 0 0;
-  }
-`;
-
-export default Signup;
+export default ResetPassword;
